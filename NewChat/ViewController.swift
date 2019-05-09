6860c1da55f1658cc3673a57eacc25bf4ac8d97e
Trọng Dương Văn <contact.trongdv@gmail.com>
2019-05-09 11:37:26 +0700

    Initial Commit

---
+++ NewChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewChat
+//
+//  Created by DuongTrong on 5/9/19.
+//  Copyright © 2019 DuongTrong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
